94532397a3cf92d766c6290b64ce064cba03b13e
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-02 23:11:57 +0600

    Created a method to add new users, also created a list of users that would be displayed when the app is loaded. Displayed key components

---
+++ src/Containers/UsersApp/UsersApp.tsx
@@ -0,0 +1,62 @@
+import UsersForm from "../../Components/UsersForm/UsersForm.tsx";
+import Users from "../../Components/Users/Users.tsx";
+import { useState } from "react";
+import { IUser } from "../../../types";
+import ToolBar from "../../Components/ToolBar/ToolBar.tsx";
+
+const UsersApp = () => {
+  const [users, setUsers] = useState<IUser[]>([
+    {
+      id: "1",
+      name: "Jane",
+      email: "example-jane@gmail.com",
+      isActive: true,
+      role: "admin",
+    },
+    {
+      id: "2",
+      name: "John",
+      email: "example-john@gmail.com",
+      isActive: true,
+      role: "editor",
+    },
+    {
+      id: "3",
+      name: "Maria",
+      email: "example-maria@gmail.com",
+      isActive: true,
+      role: "user",
+    },
+    {
+      id: "4",
+      name: "Olivia",
+      email: "example-olivia@gmail.com",
+      isActive: true,
+      role: "editor",
+    },
+  ]);
+
+  const addUser = (newUser: IUser): void => {
+    setUsers((prevState) => [newUser, ...prevState]);
+  };
+
+  return (
+    <>
+      <header>
+        <ToolBar />
+      </header>
+      <div className="container">
+        <div className="row mt-5 justify-content-between">
+          <div className="col-6">
+            <UsersForm addUser={addUser} />
+          </div>
+          <div className="col-6">
+            <Users users={users} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default UsersApp;
