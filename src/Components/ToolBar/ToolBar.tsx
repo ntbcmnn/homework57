624af800c2f085b4d71f2a615d3e163d2ff1d8da
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-02 22:59:54 +0600

    Created component to make the page look complete

---
+++ src/Components/ToolBar/ToolBar.tsx
@@ -0,0 +1,11 @@
+const ToolBar = () => {
+  return (
+    <nav className="navbar bg-dark">
+      <div className="container">
+        <span className="navbar-brand mb-0 h1 fs-1 text-white">U</span>
+      </div>
+    </nav>
+  );
+};
+
+export default ToolBar;
