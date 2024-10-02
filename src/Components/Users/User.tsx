603f5a9e29369341b4d4b24c1038069e4b366fdd
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-02 23:27:39 +0600

    Added a field to display info about the activeness status of user

--- src/Components/Users/User.tsx
+++ src/Components/Users/User.tsx
@@ -10,7 +10,8 @@ const User: React.FC<Props> = ({ user }) => {
     <div className="card col-12 mb-3 p-4">
       <h5 className="card-title">{user.name}</h5>
       <p className="card-text">{user.email}</p>
-      <p className="card-text">Role: {user.role}</p>
+      <p className="card-text">Role: <b>{user.role}</b></p>
+      <p className="card-text">Status: {user.isActive ? <b>active</b> : <b>inactive</b>}</p>
     </div>
   );
 };
